4f4d9d04cb892d10c2a14469f6164b73a3d0cb8f
Zeroooo0 <79262711+Zeroooo0@users.noreply.github.com>
2022-03-06 20:24:00 +0100

    change database

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'yourcraf_wp691' );
 define( 'DB_PASSWORD', '7Dpz@.H83S' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '82.163.176.14' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
